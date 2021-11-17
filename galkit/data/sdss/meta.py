fb4d53a7b318c6458a0c764f0c283dd5100d66f6
Brandon Bergerud <bergie1001@gmail.com>
2021-11-17 15:11:11 -0600

    added mean nMgyPerCount dictionary variable

--- galkit/data/sdss/meta.py
+++ galkit/data/sdss/meta.py
@@ -3,10 +3,22 @@ Storage of metadata related to SDSS
 
 Variables
 ---------
+sdss_nMgyPerCount : Dict[str, float]
+    Dictionary whose key represents the SDSS filter band and whose value
+    is the mean nanomaggies / count.
+
 sdss_λ / sdss_wavelengths : Dict[str, float]
     Dictionary whose key represents the SDSS filter band and whose value
     contains the effective wavelength of the filter band in meters.
 """
+
+sdss_nMgyPerCount = {
+    'u': 0.0101,
+    'g': 0.0038,
+    'r': 0.0051,
+    'i': 0.0066,
+    'z': 0.0326,
+}
 
 sdss_λ = sdss_wavelengths = {
     'u': 3543e-10,
